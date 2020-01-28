5f537b60727f927417b4ecaa591110f0317f4949
elizabethclive <47164567+elizabethclive@users.noreply.github.com>
2020-01-27 22:40:58 -0700

    everything works

--- fermat.py
+++ fermat.py
@@ -1,15 +1,15 @@
 import random
+import math
 
 
 def prime_test(N, k):
-    # This is the main function connected to the Test button. You don't need to touch it.
     return run_fermat(N,k), run_miller_rabin(N,k)
 
 
 def mod_exp(x, y, N):
     if y == 0:
         return 1
-    z = modexp(x, math.floor(y/2), N)
+    z = mod_exp(x, math.floor(y/2), N)
     if y % 2 == 0:
         return (z**2) % N
     else:
@@ -17,7 +17,6 @@ def mod_exp(x, y, N):
 
 
 def fprobability(k):
-    # ACCOUNT FOR REPEAT TESTS???????????????????
     return 1 - 0.5**k * k
 
 
@@ -28,8 +27,8 @@ def mprobability(k):
 def run_fermat(N,k):
     randomList = random.sample(range(2, N-2), k)
     for a in randomList:
-        # a = random.randint(2, N-2)
-        if a**(N-1) % N != 1:
+        # if a**(N-1) % N != 1:
+        if mod_exp(a, N-1, N) != 1:
             return 'composite'
     return 'prime'
 
@@ -46,7 +45,8 @@ def run_miller_rabin(N,k):
 
     randomList = random.sample(range(2, N-2), k)
     for a in randomList:
-        x = a**d % N
+        # x = a**d % N
+        x = mod_exp(a, d, N)
         if x == 1 or x == N - 1:
             continue
         for i in range(r-1):
