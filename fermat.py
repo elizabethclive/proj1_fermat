ae6dfccda385544765de55823580aeb5422a15cd
elizabethclive <47164567+elizabethclive@users.noreply.github.com>
2020-01-28 16:21:18 -0700

    update

--- fermat.py
+++ fermat.py
@@ -6,7 +6,7 @@ def prime_test(N, k):
     return run_fermat(N,k), run_miller_rabin(N,k)
 
 
-def mod_exp(x, y, N):
+def mod_exp(x, y, N): # Time complexity: O(log(y)) Space complexity: O(1)
     if y == 0:
         return 1
     z = mod_exp(x, math.floor(y/2), N)
@@ -16,46 +16,47 @@ def mod_exp(x, y, N):
         return (x * z**2) % N
 
 
-def fprobability(k):
+def fprobability(k): # Time complexity: O(1) Space complexity: O(1)                     # time complexity????
     return 1 - 0.5**k * k
 
 
-def mprobability(k):
+def mprobability(k): # Time complexity: O(1) Space complexity: O(1)
     return 1 - 0.75**k * k
 
 
-def run_fermat(N,k):
+def run_fermat(N,k): # Time complexity: O(k+log(N-1)) Space complexity: O(k)            # Check
+    # create a random list of numbers to test primality
     randomList = random.sample(range(2, N-2), k)
+
+    # test the primality using Fermat's Little Theorem
     for a in randomList:
-        # if a**(N-1) % N != 1:
         if mod_exp(a, N-1, N) != 1:
             return 'composite'
     return 'prime'
 
 
-def run_miller_rabin(N,k):
-    # You will need to implement this function and change the return value, which should be
-    # either 'prime' or 'composite'.
-    #
+def run_miller_rabin(N,k): # Time complexity: O(?????????????) Space complexity: O(k)
+    # factor out powers of 2 from N-1
     r = 0
     d = N-1
     while (d % 2 == 0):
         d = d / 2
         r += 1
 
+    # create a random list of numbers to test primality
     randomList = random.sample(range(2, N-2), k)
-    for a in randomList:
-        # x = a**d % N
+
+    # test the primality using the Miller-Rabin primality test
+    for a in randomList:                                                                # O(k)
         x = mod_exp(a, d, N)
         if x == 1 or x == N - 1:
             continue
-        for i in range(r-1):
-            x = x**2 % N
+        return_composite = True
+        for i in range(r-1):                                                            # worst case, huge??? best case, doesn't even run
+            x = mod_exp(x, 2, N)                                                        # O(log(N-1))
             if x == N - 1:
+                return_composite = False
                 continue
-        return 'composite'
+        if return_composite:
+            return 'composite'
     return 'prime'
-    # To generate random values for a, you will most likely want to use
-    # random.randint(low,hi) which gives a random integer between low and
-    #  hi, inclusive.
-    # return 'composite'
